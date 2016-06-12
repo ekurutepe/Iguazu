8a55e64de57a03414c654ddc8b05e1d2bb36b27c
Engin Kurutepe <engin@kurutepe.com>
2016-06-12 19:53:10 +0200

    Initial Commit

---
+++ Iguazu/Iguazu.h
@@ -0,0 +1,19 @@
+//
+//  Iguazu.h
+//  Iguazu
+//
+//  Created by Engin Kurutepe on 12/06/16.
+//  Copyright © 2016 Fifteen Jugglers Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Iguazu.
+FOUNDATION_EXPORT double IguazuVersionNumber;
+
+//! Project version string for Iguazu.
+FOUNDATION_EXPORT const unsigned char IguazuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Iguazu/PublicHeader.h>
+
+
