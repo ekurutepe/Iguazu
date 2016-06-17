43f2884fbf3532cd0de3bc32b04caaaf0d12e90c
Engin Kurutepe <engin@kurutepe.com>
2016-06-17 21:44:15 +0200

    parse I-extensions

--- Iguazu/IGCExtension.swift
+++ Iguazu/IGCExtension.swift
@@ -8,7 +8,44 @@
 
 import Foundation
 
-enum IGCExtension {
-    case fixAccuracy(startIndex:Int, finalIndex:Int, accuracy: Int)
-    case engineNoiseLevel(startIndex:Int, finalIndex:Int, noiseLevel: Int)
+struct IGCExtension {
+    
+    enum ExtensionType: String {
+        case fixAccuracy = "FXA"
+        case engineNoiseLevel = "ENL"
+    }
+    
+    let startIndex: Int
+    let endIndex: Int
+    let type: ExtensionType
+    
+    static func parseExtensions(line: String) -> [IGCExtension]? {
+        let countIndex = line.index(after: line.startIndex)
+        let firstChar = line.substring(to: countIndex)
+        
+        guard firstChar == "I" || firstChar == "J" else { return nil }
+        
+        let extensionStartIndex = line.index(countIndex, offsetBy: 2)
+        guard let extensionCount = Int(line.substring(with: countIndex..<extensionStartIndex)) else { return nil }
+        
+        let extensionCharCount = 7
+        
+        var extensions = [IGCExtension]()
+        
+        for i in 0..<extensionCount {
+            let firstByteIndex = line.index(extensionStartIndex, offsetBy: i*extensionCharCount)
+            let secondByteIndex = line.index(firstByteIndex, offsetBy: 2)
+            let codeIndex = line.index(secondByteIndex, offsetBy: 2)
+            
+            guard let firstByte = Int(line.substring(with: firstByteIndex..<secondByteIndex)) else { break }
+            guard let secondByte = Int(line.substring(with: secondByteIndex..<codeIndex)) else { break }
+            guard let type = ExtensionType(rawValue: line.substring(with: codeIndex..<line.index(codeIndex, offsetBy: 3))) else { break }
+            
+            let ext = IGCExtension(startIndex: firstByte, endIndex: secondByte, type: type)
+            
+            extensions.append(ext)
+        }
+        
+        return extensions
+    }
 }
---
+++ IguazuTests/IGCExtensionTests.swift
@@ -0,0 +1,43 @@
+//
+//  IGCExtensionTests.swift
+//  Iguazu
+//
+//  Created by Engin Kurutepe on 17/06/2016.
+//  Copyright © 2016 Fifteen Jugglers Software. All rights reserved.
+//
+
+import XCTest
+@testable import Iguazu
+
+class IGCExtensionTests: XCTestCase {
+    
+    let iLine = "I023638FXA3941ENL"
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testSimpleILine() {
+        guard let extensions = IGCExtension.parseExtensions(line: iLine) else { XCTFail("could not parse \(iLine)"); return }
+        
+        XCTAssertEqual(extensions.count, 2)
+        
+        let firstExtension = extensions[0]
+        
+        XCTAssertEqual(firstExtension.startIndex, 36)
+        XCTAssertEqual(firstExtension.endIndex, 38)
+        XCTAssertEqual(firstExtension.type, IGCExtension.ExtensionType.fixAccuracy)
+        
+        let secondExtension = extensions[1]
+        
+        XCTAssertEqual(secondExtension.startIndex, 39)
+        XCTAssertEqual(secondExtension.endIndex, 41)
+        XCTAssertEqual(secondExtension.type, IGCExtension.ExtensionType.engineNoiseLevel)
+    }
+}
