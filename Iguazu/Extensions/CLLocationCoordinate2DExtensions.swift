cd16846e9b22c8c5b30e6212aa9333354d5f1fe5
Engin Kurutepe <engin@kurutepe.com>
2019-09-05 15:25:51 +0200

    Revert "better accuracy for coordinate(at:direction:)"
    This reverts commit 40953fbfd87cafe35721c14c126ee2a21f88f2e4.

--- Iguazu/Extensions/CLLocationCoordinate2DExtensions.swift
+++ Iguazu/Extensions/CLLocationCoordinate2DExtensions.swift
@@ -8,78 +8,63 @@
 
 import CoreLocation
 
-private extension Double {
-  var inRadians: Double {
-    return self * .pi / 180.0
-  }
-
-  var inDegrees: CLLocationDegrees {
-    return self * 180.0 / .pi
-  }
-}
-
-public extension CLLocationCoordinate2D {
-  private func DegreesToRadians(_ degrees: Double) -> Double { return degrees * .pi / 180.0 }
-  private func RadiansToDegrees(_ radians: Double) -> CLLocationDegrees { return radians * 180.0 / .pi }
-
-  // calculations taken from http://gis.stackexchange.com/questions/75528/length-of-a-degree-where-do-the-terms-in-this-formula-come-from
-  var metersPerLatitudeDegree: CLLocationDistance {
-    let m1 = 111132.92;     // latitude calculation term 1
-    let m2 = -559.82;       // latitude calculation term 2
-    let m3 = 1.175;         // latitude calculation term 3
-    let m4 = -0.0023;       // latitude calculation term 4
-
-    return m1 + m2 * cos(2 * latitude) + m3 * cos(4 * latitude) + m4 * cos(6 * latitude)
-  }
-
-  var metersPerLongitudeDegree: CLLocationDistance {
-    let p1 = 111412.84     // longitude calculation term 1
-    let p2 = -93.5         // longitude calculation term 2
-    let p3 = 0.118         // longitude calculation term 3
-
-    return p1 * cos(latitude) + p2 * cos(3 * latitude) + p3 * cos(5 * latitude)
-  }
-
-  func distance(from coord: CLLocationCoordinate2D) -> CLLocationDistance {
-    let loc1 = CLLocation(latitude: self.latitude, longitude: self.longitude)
-    let loc2 = CLLocation(latitude: coord.latitude, longitude: coord.longitude)
-
-    return loc1.distance(from: loc2)
-  }
-
-  func bearing(to coord:CLLocationCoordinate2D) -> CLLocationDegrees {
-    let lat1 = DegreesToRadians(self.latitude);
-    let lon1 = DegreesToRadians(self.longitude);
-
-    let lat2 = DegreesToRadians(coord.latitude);
-    let lon2 = DegreesToRadians(coord.longitude);
-
-    let dLon = lon2 - lon1;
-
-    let y = sin(dLon) * cos(lat2);
-    let x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon);
-    let radiansBearing = atan2(y, x);
-    let degrees = RadiansToDegrees(radiansBearing)
-    return degrees > 0.0 ? degrees : degrees + 360.0;
-  }
-
-  func coordinate(at distance: CLLocationDistance, direction: CLLocationDirection) -> CLLocationCoordinate2D {
-    let latInRad = DegreesToRadians(self.latitude)
-    let lngInRad = DegreesToRadians(self.longitude)
-    let angularDistanceLat = distance/LocationConstants.metersPerRadianLat
-    let angularDistanceLon = distance/LocationConstants.metersPerRadianLon
-    let bearingInRad = DegreesToRadians(direction)
-
-    let lat2InRad = asin( sin(latInRad)*cos(angularDistanceLat) + cos(latInRad)*sin(angularDistanceLat)*cos(bearingInRad) )
-    let lng2InRad = lngInRad + atan2(sin(bearingInRad)*sin(angularDistanceLon)*cos(latInRad), cos(angularDistanceLon)-sin(latInRad)*sin(lat2InRad));
-
-    let coord = CLLocationCoordinate2D(latitude: RadiansToDegrees(lat2InRad), longitude: RadiansToDegrees(lng2InRad))
-
-    return coord
-  }
-}
-
-struct LocationConstants {
-  static let metersPerRadianLat: CLLocationDistance = 6373000.0
-  static let metersPerRadianLon: CLLocationDistance = 5602900.0
+extension CLLocationCoordinate2D {
+    private func DegreesToRadians(_ degrees: Double) -> Double { return degrees * .pi / 180.0 }
+    private func RadiansToDegrees(_ radians: Double) -> CLLocationDegrees { return radians * 180.0 / .pi }
+    
+    // calculations taken from http://gis.stackexchange.com/questions/75528/length-of-a-degree-where-do-the-terms-in-this-formula-come-from
+    var metersPerLatitudeDegree: CLLocationDistance {
+        let m1 = 111132.92;     // latitude calculation term 1
+        let m2 = -559.82;       // latitude calculation term 2
+        let m3 = 1.175;         // latitude calculation term 3
+        let m4 = -0.0023;       // latitude calculation term 4
+        
+        return m1 + m2 * cos(2 * latitude) + m3 * cos(4 * latitude) + m4 * cos(6 * latitude)
+    }
+    
+    var metersPerLongitudeDegree: CLLocationDistance {
+        let p1 = 111412.84     // longitude calculation term 1
+        let p2 = -93.5         // longitude calculation term 2
+        let p3 = 0.118         // longitude calculation term 3
+        
+        return p1 * cos(latitude) + p2 * cos(3 * latitude) + p3 * cos(5 * latitude)
+    }
+    
+    func distance(from coord: CLLocationCoordinate2D) -> CLLocationDistance {
+        let loc1 = CLLocation(latitude: self.latitude, longitude: self.longitude)
+        let loc2 = CLLocation(latitude: coord.latitude, longitude: coord.longitude)
+        
+        return loc1.distance(from: loc2)
+    }
+    
+    func bearing(to coord:CLLocationCoordinate2D) -> CLLocationDegrees {
+        let lat1 = DegreesToRadians(self.latitude);
+        let lon1 = DegreesToRadians(self.longitude);
+        
+        let lat2 = DegreesToRadians(coord.latitude);
+        let lon2 = DegreesToRadians(coord.longitude);
+        
+        let dLon = lon2 - lon1;
+        
+        let y = sin(dLon) * cos(lat2);
+        let x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon);
+        let radiansBearing = atan2(y, x);
+        let degrees = RadiansToDegrees(radiansBearing)
+        return degrees > 0.0 ? degrees : degrees + 360.0;
+    }
+    
+    func coordinate(at distance: CLLocationDistance, direction: CLLocationDirection) -> CLLocationCoordinate2D {
+        let latInRad = DegreesToRadians(self.latitude)
+        let lngInRad = DegreesToRadians(self.longitude)
+        let R = 6371000.0
+        let angularDistance = distance/R
+        let bearingInRad = DegreesToRadians(direction)
+        
+        let lat2InRad = asin( sin(latInRad)*cos(angularDistance) + cos(latInRad)*sin(angularDistance)*cos(bearingInRad) )
+        let lng2InRad = lngInRad + atan2(sin(bearingInRad)*sin(angularDistance)*cos(latInRad), cos(angularDistance)-sin(latInRad)*sin(lat2InRad));
+        
+        let coord = CLLocationCoordinate2D(latitude: RadiansToDegrees(lat2InRad), longitude: RadiansToDegrees(lng2InRad))
+        
+        return coord
+    }
 }
