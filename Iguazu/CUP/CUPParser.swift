88d51804c7b361463651af9b0fdfc74b9b8a42cc
Engin Kurutepe <engin@kurutepe.com>
2020-03-06 22:06:08 +0100

    harden CUP parsing

--- Iguazu/CUP/CUPParser.swift
+++ Iguazu/CUP/CUPParser.swift
@@ -9,15 +9,34 @@ import Foundation
 
 public final class CUPParser {
     static public func pointOfInterest(from row: String, sourceIdentifier: String?) -> PointOfInterest? {
-        let components = row.components(separatedBy: ",")
+        func oddNumberOfQuotes(_ string: String) -> Bool {
+            return string.components(separatedBy: "\"").count % 2 == 0
+        }
+
+        let rawComponents = row.components(separatedBy: ",")
+
+        // Adapted from https://github.com/Daniel1of1/CSwiftV/blob/develop/Sources/CSwiftV/CSwiftV.swift
+        var components = [String]()
+        for newString in rawComponents {
+            guard let record = components.last , oddNumberOfQuotes(record) == true else {
+                components.append(newString)
+                continue
+            }
+            components.removeLast()
+            let lastElem = record + "," + newString
+            components.append(lastElem)
+        }
+
         //    "001SPLuesse",,XX,5208.650N,01240.100E,66m,5,,,,
         guard
-            components.count == 11,
+            components.count >= 11,
             let latitude = Measurement<UnitAngle>(latitudeString: components[3])?.value,
-            let longitude = Measurement<UnitAngle>(longitudeString: components[4])?.value,
-            let elevation = Measurement<UnitLength>(components[5])
-        else { return nil }
+            let longitude = Measurement<UnitAngle>(longitudeString: components[4])?.value
+        else {
+            return nil
+        }
 
+        let elevation = Measurement<UnitLength>(components[5]) ?? Measurement<UnitLength>("0m")!
         var p = PointOfInterest(
             title: components[0].replacingOccurrences(of: "\"", with: ""),
             code: components[1].replacingOccurrences(of: "\"", with: ""),
@@ -45,9 +64,10 @@ public extension CUPFile {
         self.name = name
         do {
             let fileContents = try String(contentsOf: fileURL)
-            let lines = fileContents.components(separatedBy: .newlines)
+            let lines = Array(fileContents.components(separatedBy: .newlines).dropFirst())
             self.points = lines.concurrentMap { (line) -> PointOfInterest? in
-                    CUPParser.pointOfInterest(from: line, sourceIdentifier: fileURL.lastPathComponent)
+                    guard !line.isEmpty else { return nil }
+                    return CUPParser.pointOfInterest(from: line, sourceIdentifier: fileURL.lastPathComponent)
                 }
                 .compactMap { $0 }
         } catch {
